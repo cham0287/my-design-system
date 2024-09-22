994feb2a8882bb035de97a274c7a8a0259784869
cham0287 <crtmt97@gmail.com>
2024-09-23 00:03:17 +0900

    feat: Label 컴포넌트 forwardRef로 변경

--- src/components/Label.tsx
+++ src/components/Label.tsx
@@ -1,12 +1,20 @@
-import { ReactNode } from 'react';
+import { ReactNode, forwardRef } from 'react';
 
 type LabelHTMLAttributes = React.LabelHTMLAttributes<HTMLLabelElement>;
 interface RootProps extends LabelHTMLAttributes {
   children: ReactNode;
 }
 
-const Root = ({ children, ...props }: RootProps) => {
-  return <label {...props}>{children}</label>;
-};
+const Root = forwardRef<HTMLLabelElement, RootProps>(
+  ({ children, ...props }, ref) => {
+    return (
+      <label ref={ref} {...props}>
+        {children}
+      </label>
+    );
+  }
+);
+
+Root.displayName = 'Root';
 
 export { Root };
