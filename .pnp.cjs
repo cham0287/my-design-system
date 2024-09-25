fddbd1796354087075ebecf9151837c0af27d675
cham0287 <crtmt97@gmail.com>
2024-09-25 23:43:13 +0900

    build: esm, cjs 각각 생성하도록 설정 수정

--- .pnp.cjs
+++ .pnp.cjs
@@ -37,6 +37,7 @@ const RAW_RUNTIME_STATE =
           ["@storybook/react", "virtual:c150bec6cf715e9542b20b32709127b50d0390c596c9758ce19db72bfdf043c9deb42bcb1f75aff449f4119dd6544eeba949b4d5f45b4d99a25e7270884f5bb3#npm:8.2.9"],\
           ["@storybook/react-vite", "virtual:c150bec6cf715e9542b20b32709127b50d0390c596c9758ce19db72bfdf043c9deb42bcb1f75aff449f4119dd6544eeba949b4d5f45b4d99a25e7270884f5bb3#npm:8.2.9"],\
           ["@storybook/test", "virtual:c150bec6cf715e9542b20b32709127b50d0390c596c9758ce19db72bfdf043c9deb42bcb1f75aff449f4119dd6544eeba949b4d5f45b4d99a25e7270884f5bb3#npm:8.2.9"],\
+          ["@types/node", "npm:22.7.0"],\
           ["@types/react", "npm:18.3.5"],\
           ["@types/react-dom", "npm:18.3.0"],\
           ["@vitejs/plugin-react-swc", "virtual:c150bec6cf715e9542b20b32709127b50d0390c596c9758ce19db72bfdf043c9deb42bcb1f75aff449f4119dd6544eeba949b4d5f45b4d99a25e7270884f5bb3#npm:3.7.0"],\
@@ -51,6 +52,7 @@ const RAW_RUNTIME_STATE =
           ["react-dom", "virtual:c150bec6cf715e9542b20b32709127b50d0390c596c9758ce19db72bfdf043c9deb42bcb1f75aff449f4119dd6544eeba949b4d5f45b4d99a25e7270884f5bb3#npm:18.3.1"],\
           ["storybook", "npm:8.2.9"],\
           ["tailwindcss", "npm:3.4.10"],\
+          ["terser", "npm:5.33.0"],\
           ["typescript", "patch:typescript@npm%3A5.5.4#optional!builtin<compat/typescript>::version=5.5.4&hash=379a07"],\
           ["typescript-eslint", "virtual:c150bec6cf715e9542b20b32709127b50d0390c596c9758ce19db72bfdf043c9deb42bcb1f75aff449f4119dd6544eeba949b4d5f45b4d99a25e7270884f5bb3#npm:8.4.0"],\
           ["vite", "virtual:c150bec6cf715e9542b20b32709127b50d0390c596c9758ce19db72bfdf043c9deb42bcb1f75aff449f4119dd6544eeba949b4d5f45b4d99a25e7270884f5bb3#npm:5.4.3"]\
@@ -3039,6 +3041,17 @@ const RAW_RUNTIME_STATE =
         "linkType": "HARD"\
       }]\
     ]],\
+    ["@jridgewell/source-map", [\
+      ["npm:0.3.6", {\
+        "packageLocation": "../../../.yarn/berry/cache/@jridgewell-source-map-npm-0.3.6-fe0849eb05-10c0.zip/node_modules/@jridgewell/source-map/",\
+        "packageDependencies": [\
+          ["@jridgewell/source-map", "npm:0.3.6"],\
+          ["@jridgewell/gen-mapping", "npm:0.3.5"],\
+          ["@jridgewell/trace-mapping", "npm:0.3.25"]\
+        ],\
+        "linkType": "HARD"\
+      }]\
+    ]],\
     ["@jridgewell/sourcemap-codec", [\
       ["npm:1.5.0", {\
         "packageLocation": "../../../.yarn/berry/cache/@jridgewell-sourcemap-codec-npm-1.5.0-dfd9126d71-10c0.zip/node_modules/@jridgewell/sourcemap-codec/",\
@@ -4747,6 +4760,14 @@ const RAW_RUNTIME_STATE =
           ["undici-types", "npm:6.19.8"]\
         ],\
         "linkType": "HARD"\
+      }],\
+      ["npm:22.7.0", {\
+        "packageLocation": "../../../.yarn/berry/cache/@types-node-npm-22.7.0-ec7f359a48-10c0.zip/node_modules/@types/node/",\
+        "packageDependencies": [\
+          ["@types/node", "npm:22.7.0"],\
+          ["undici-types", "npm:6.19.8"]\
+        ],\
+        "linkType": "HARD"\
       }]\
     ]],\
     ["@types/prop-types", [\
@@ -6012,6 +6033,13 @@ const RAW_RUNTIME_STATE =
       }]\
     ]],\
     ["commander", [\
+      ["npm:2.20.3", {\
+        "packageLocation": "../../../.yarn/berry/cache/commander-npm-2.20.3-d8dcbaa39b-10c0.zip/node_modules/commander/",\
+        "packageDependencies": [\
+          ["commander", "npm:2.20.3"]\
+        ],\
+        "linkType": "HARD"\
+      }],\
       ["npm:4.1.1", {\
         "packageLocation": "../../../.yarn/berry/cache/commander-npm-4.1.1-22a0fe921b-10c0.zip/node_modules/commander/",\
         "packageDependencies": [\
@@ -8643,6 +8671,7 @@ const RAW_RUNTIME_STATE =
           ["@storybook/react", "virtual:c150bec6cf715e9542b20b32709127b50d0390c596c9758ce19db72bfdf043c9deb42bcb1f75aff449f4119dd6544eeba949b4d5f45b4d99a25e7270884f5bb3#npm:8.2.9"],\
           ["@storybook/react-vite", "virtual:c150bec6cf715e9542b20b32709127b50d0390c596c9758ce19db72bfdf043c9deb42bcb1f75aff449f4119dd6544eeba949b4d5f45b4d99a25e7270884f5bb3#npm:8.2.9"],\
           ["@storybook/test", "virtual:c150bec6cf715e9542b20b32709127b50d0390c596c9758ce19db72bfdf043c9deb42bcb1f75aff449f4119dd6544eeba949b4d5f45b4d99a25e7270884f5bb3#npm:8.2.9"],\
+          ["@types/node", "npm:22.7.0"],\
           ["@types/react", "npm:18.3.5"],\
           ["@types/react-dom", "npm:18.3.0"],\
           ["@vitejs/plugin-react-swc", "virtual:c150bec6cf715e9542b20b32709127b50d0390c596c9758ce19db72bfdf043c9deb42bcb1f75aff449f4119dd6544eeba949b4d5f45b4d99a25e7270884f5bb3#npm:3.7.0"],\
@@ -8657,6 +8686,7 @@ const RAW_RUNTIME_STATE =
           ["react-dom", "virtual:c150bec6cf715e9542b20b32709127b50d0390c596c9758ce19db72bfdf043c9deb42bcb1f75aff449f4119dd6544eeba949b4d5f45b4d99a25e7270884f5bb3#npm:18.3.1"],\
           ["storybook", "npm:8.2.9"],\
           ["tailwindcss", "npm:3.4.10"],\
+          ["terser", "npm:5.33.0"],\
           ["typescript", "patch:typescript@npm%3A5.5.4#optional!builtin<compat/typescript>::version=5.5.4&hash=379a07"],\
           ["typescript-eslint", "virtual:c150bec6cf715e9542b20b32709127b50d0390c596c9758ce19db72bfdf043c9deb42bcb1f75aff449f4119dd6544eeba949b4d5f45b4d99a25e7270884f5bb3#npm:8.4.0"],\
           ["vite", "virtual:c150bec6cf715e9542b20b32709127b50d0390c596c9758ce19db72bfdf043c9deb42bcb1f75aff449f4119dd6544eeba949b4d5f45b4d99a25e7270884f5bb3#npm:5.4.3"]\
@@ -10514,6 +10544,19 @@ const RAW_RUNTIME_STATE =
         "linkType": "HARD"\
       }]\
     ]],\
+    ["terser", [\
+      ["npm:5.33.0", {\
+        "packageLocation": "../../../.yarn/berry/cache/terser-npm-5.33.0-58f8b7f7c3-10c0.zip/node_modules/terser/",\
+        "packageDependencies": [\
+          ["terser", "npm:5.33.0"],\
+          ["@jridgewell/source-map", "npm:0.3.6"],\
+          ["acorn", "npm:8.12.1"],\
+          ["commander", "npm:2.20.3"],\
+          ["source-map-support", "npm:0.5.21"]\
+        ],\
+        "linkType": "HARD"\
+      }]\
+    ]],\
     ["text-table", [\
       ["npm:0.2.0", {\
         "packageLocation": "../../../.yarn/berry/cache/text-table-npm-0.2.0-d92a778b59-10c0.zip/node_modules/text-table/",\
@@ -11057,7 +11100,7 @@ const RAW_RUNTIME_STATE =
           ["vite", "virtual:c150bec6cf715e9542b20b32709127b50d0390c596c9758ce19db72bfdf043c9deb42bcb1f75aff449f4119dd6544eeba949b4d5f45b4d99a25e7270884f5bb3#npm:5.4.3"],\
           ["@types/less", null],\
           ["@types/lightningcss", null],\
-          ["@types/node", null],\
+          ["@types/node", "npm:22.7.0"],\
           ["@types/sass", null],\
           ["@types/sass-embedded", null],\
           ["@types/stylus", null],\
@@ -11073,7 +11116,7 @@ const RAW_RUNTIME_STATE =
           ["sass-embedded", null],\
           ["stylus", null],\
           ["sugarss", null],\
-          ["terser", null]\
+          ["terser", "npm:5.33.0"]\
         ],\
         "packagePeers": [\
           "@types/less",\
---
+++ src/components/index.ts
@@ -0,0 +1,1 @@
+export { Root as Label } from './Label';
